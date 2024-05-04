c184f2d52e3ad6f511e74ebc17e2cd2b0b8b4787
Tonmay1555 <ahmmedtonmay1555@gmail.com>
2024-05-04 14:14:05 +0600

    added a git hub login

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,10 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCV8eYzmgMvNPxSMhI6bFns6ozKbLURp68",
   authDomain: "world-explore-client.firebaseapp.com",
@@ -14,7 +11,7 @@ const firebaseConfig = {
   appId: "1:40762195808:web:560d533b439b130c1e2805"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 export default auth;
